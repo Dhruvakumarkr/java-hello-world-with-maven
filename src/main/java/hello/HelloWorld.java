af855b90225e82f396e9a2c768195f0e9c6f0cf2
Dhruva kumar kr <85985988+Dhruvakumarkr@users.noreply.github.com>
2024-03-23 14:01:59 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time in india: " + currentTime);
+		  System.out.println("The current local time in karnataka: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
